f48cde899a47a09b2fa720c98773297c7c9bd613
didier <didier@didier>
2022-10-20 11:08:26 +0200

    td5

--- TD2/src/atelier02/B.java
+++ TD2/src/atelier02/B.java
@@ -1,5 +1,20 @@
 package atelier02;
-
+/**
+ * 
+ * @author didier
+ *
+ */
 public class B {
-
+	/**
+	 * retourne la valeur n/2 si un entier n passé en paramètre est divisible
+	 * par 13
+	 * @param n entier 
+	 * @return n/2 si divisible par 13 sinon n+2
+	 */
+	public static int divise(int n) {
+		if (A.treize(n) == true)
+				return n/2;
+		else
+				return n + 2;
+	}
 }
