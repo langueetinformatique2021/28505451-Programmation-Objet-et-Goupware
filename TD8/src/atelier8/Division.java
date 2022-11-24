77cc6f945c22e8581bb2f369655e7caf8b80c513
didier <didier@didier>
2022-11-24 10:03:36 +0100

    question 1 exception division par zéro

---
+++ TD8/src/atelier8/Division.java
@@ -0,0 +1,15 @@
+package atelier8;
+/**
+ * exception prédéfinie 
+ * @return l'exception division par zéro 
+ *
+ */
+public class Division {
+	public static void main(String args [])
+	{
+		int i = 1000, j;
+		do {
+			i--;
+			j = 1/i; } while (true);
+	}
+}
